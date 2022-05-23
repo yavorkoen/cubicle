fc8880bec55262be9c87a9b70e2644dfca83b1ed
yavorkoen <yavorkoen@gmail.com>
2022-05-23 13:37:40 +0300

    run app

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!<h1>');
+})
 
-app.listen(3001)
+app.listen(3000, console.log.bind(console, 'App running on port 3000...'));
