7d9a7fe746112617f877f73abd3f75205232a627
yavorkoen <yavorkoen@gmail.com>
2022-05-23 14:02:16 +0300

    adding layout

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-    res.render('index', {layout: false});
+    res.render('index');
 })
 
 app.listen(3000, console.log.bind(console, 'App running on port 3000...'));
