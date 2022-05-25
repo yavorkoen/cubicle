2a3681a00c39524d61f80f6753b31d2a7d6a39a4
yavorkoen <yavorkoen@gmail.com>
2022-05-25 12:07:25 +0300

    add attach accessory page
    with dynamic info

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,8 +1,12 @@
 const router = require('express').Router({ mergeParams: true});
+const cubeService = require('../services/cubeService.js');
 
-const getAddAccessoryPage = (req, res) => {
-    console.log('accessory');
-    res.render('cube/accessory/attach');
+
+const getAddAccessoryPage = async (req, res) => {
+    
+    const cube = await cubeService.getOne(req.params.cubeId);
+    console.log(cube);
+    res.render('cube/accessory/attach', { ...cube });
 }
 
 router.get('/add', getAddAccessoryPage);
