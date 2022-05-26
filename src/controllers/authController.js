83129f4ff29e8a3ef34ca3f5a900e9223c7f5523
yavorkoen <yavorkoen@gmail.com>
2022-05-26 12:57:47 +0300

    added register page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,7 +11,19 @@ const login = (req, res) => {
     res.redirect('/login');
 }
 
+const renderRegisterPage = (req, res) => {
+    res.render('auth/register', {layout: 'unsigned'});
+}
+
+const register = (req, res) => {
+    console.log(req.body);
+
+    res.redirect('/register');
+}
+
 router.get('/login', renderLoginPage);
 router.post('/login', login)
+router.get('/register', renderRegisterPage);
+router.post('/register', register);
 
 module.exports = router;
