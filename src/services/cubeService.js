648db47af2862b5eda3570a3a6c21ca659ac62cd
yavorkoen <yavorkoen@gmail.com>
2022-05-26 10:40:24 +0300

    fixed search

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -25,13 +25,24 @@ const create = (name, description, imageUrl, difficultyLevel, accessories) => {
     return cube.save();
 }
 
-const search = (search, from, to) => {
-    return Cube.find({}).lean()
-        
+const search = async (search, from, to) => {
+    let foundCubes = await Cube.find({}).lean();
+
+    if (search) {
+        foundCubes = foundCubes.filter(x => x.name.toLowerCase().includes(search.toLowerCase()));
+    }
+    if (from > 0 && from < 7) {
+        foundCubes = foundCubes.filter(x => x.difficultyLevel >= from);
+    }
+    if (to > 0 && to >= from && to < 7) {
+        foundCubes = foundCubes.filter(x => x.difficultyLevel <= to);
+
+    }
+    return foundCubes;
 }
 
 const attachAccessory = async (cubeId, accessoryId) => {
-    let cube =  await Cube.findById(cubeId);
+    let cube = await Cube.findById(cubeId);
     let accessory = await Accessory.findById(accessoryId);
 
     cube.accessories.push(accessory);
