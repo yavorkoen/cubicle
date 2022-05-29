dbc88e36de94cd5c99f8cd3e753df7b58c6dcfef
yavorkoen <yavorkoen@gmail.com>
2022-05-29 18:59:14 +0300

    render delete/edit pages

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -5,7 +5,7 @@ const cubeAccessoryController = require('../controllers/cubeAccessoryController.
 
 const renderCreatePage = (req, res) => {
     res.render('cube/create');
-}
+};
 
 const createCube = async (req, res) => {
     let { name, description, imageUrl, difficultyLevel } = req.body;
@@ -16,19 +16,32 @@ const createCube = async (req, res) => {
     } catch (err) {
         res.status(400).send(err.message);
     }
-
-
-}
+};
 
 const details = async (req, res) => {
     const cube = await cubeService.getOne(req.params.cubeId);
     res.render('cube/details', { ...cube })
-}
+};
+
+const renderEditPage = (req, res) => {
+    
+    res.render('cube/edit')
+};
+
+
+
+
+const renderDeletePage = (req, res) => {
+    
+    res.render('cube/delete')
+};
 
 
 router.get('/create', renderCreatePage);
 router.post('/create', createCube);
-router.get('/details/:cubeId', details);
+router.get('/:cubeId', details);
 router.use('/:cubeId/accessory', cubeAccessoryController);
+router.get('/:cubeId/edit', renderEditPage);
+router.get('/:cubeId/delete', renderDeletePage);
 
 module.exports = router
